0fe5f0ba8be535a07dcf581892c349b4ba5d3e96
Gerald Muvengei <gerald.muvengei@volane.com>
2023-11-10 15:59:50 +0300

    Updated logos

--- components/Ui/LogoLight.vue
+++ components/Ui/LogoLight.vue
@@ -3,8 +3,8 @@ const runtimeConfig = useRuntimeConfig();
 </script>
 
 <template>
-  <NuxtLink to="/" class="rounded-lg overflow-hidden">
-    <NuxtImg class="w-24 sm:w-24 text-primary-focus font-bold text-xl" :alt="runtimeConfig?.public?.websiteName"
+  <NuxtLink to="/" class="overflow-hidden">
+    <NuxtImg class="w-56 sm:w-64 text-primary-focus font-bold text-xl" :alt="runtimeConfig?.public?.websiteName"
       src="/logo-light.png" />
   </NuxtLink>
 </template>
